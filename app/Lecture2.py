cd1152ca31c7162c98385b05dc41ea9889b802b7
Csaba Maczo <maczocsaba@gmail.com>
2025-12-02 21:47:59 +0100

    Validation

--- app/Lecture2.py
+++ app/Lecture2.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -16,6 +17,15 @@ class Book:
         self.description = description
         self.rating = rating
 
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+
+
 BOOKS = [
     Book(1, "Computer Science Pro", "codingwithroby", "A very nice book!", 5),
     Book(2, "Be fast with FastAPI", "codingwithroby", "A great book!", 5),
@@ -28,3 +38,8 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
